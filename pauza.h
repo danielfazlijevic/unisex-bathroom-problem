163b492f946c90891a724fa88feffbfd3adfbe3f
Daniel Fazlijevic <danielfazlijevic98@gmail.com>
2019-02-14 20:14:29 +0100

    pauza izmene, random pol

--- pauza.h
+++ pauza.h
@@ -2,30 +2,35 @@
 #include <unistd.h>
 #include <string.h>
 
-void pauza(int pol){
-    // random broj od 0 do 9
-    int vreme = random() % 10;
-    //  char *pol = "nepoznatog";
-    //  printf("pauza");
-    char *imePola = malloc(sizeof(char)*7);
-    if(pol == 0)
-	strcpy(imePola, "muskog");
-    else
-	strcpy(imePola, "zenskog");
-    printf("Osoba %s pola je usao/la u WC.\n", imePola);
-    // printf("Pauza: %d sekunde\n", vreme);
-    sleep(vreme);
-    printf("Osoba %s pola je izasao/la iz WC-a posle %d sekunde.\n", imePola, vreme);
-    //  printf("Osoba je provela u WC-u %d sekunde\n", vreme);
-}
+
+// funkcija koja vraca string 'muskog' ili 'zenskog' u zavisnosti od broja koje mu damo kao parametar
 char *imePola(int pol){
     char *ime = malloc(sizeof(char)*7);
     if(pol == 0)
-	strcpy(ime, "muskog");
+        strcpy(ime, "muskog");
     else
-	strcpy(ime, "zenskog");
+        strcpy(ime, "zenskog");
     return ime;
 }
+
+// funkcija koja predstavlja ulazak osobe u wc, ostajanje i izlaz
+void ulazak(int pol){
+     // random broj od 0 do 9
+    int vreme = random() % 10;
+   
+    printf("Osoba %s pola je usao/la u WC.\n", imePola(pol));
+    sleep(vreme);
+    printf("Osoba %s pola je izasao/la iz WC-a posle %d sekunde.\n", imePola(pol), vreme);
+}
+
+// funkcija za pauziranje 
+void pauza(){
+   int vreme = random() % 5;
+   // printf("Pauza: %d\n", vreme);
+   sleep(vreme);
+}
+
+// funkcija koja obavestava o dolasku osobe do WC-a
 void dolazak(int pol) {
     printf("Osoba %s pola zeli da udje u WC.\n", imePola(pol));
 }
--- unisex_bathroom.c
+++ unisex_bathroom.c
@@ -1,4 +1,5 @@
-/* * Daniel Fazlijevic br. indeksa 22/17
+/* 
+ * Daniel Fazlijevic br. indeksa 22/17
  * <danielfazlijevic98@gmail.com>
  * https://danielfazlijevic.me
  * Fakultet Organizacionih Nauka
@@ -10,7 +11,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include "osobe.h" // fajl sa prototipom funkcija za muskarce i zene
-#include "pauza.h" // header fajl sa funkcijom za random pauzu
+#include "pauza.h" // header fajl sa funkcijom za pauzu, dolaske i odlaske
 
 // maksimalni broj osoba u jednom toletu
 #define MAX_OSOBA 3
@@ -32,62 +33,74 @@ sem_t max_sem;
 int main(void){
     srand(time(NULL)); // da osiguramo prave random vrednosti 	
     printf("Unisex Bathroom problem - projektni rad\n");
+    // postavljamo broj muskaraca i broj zena na 0
     broj_muskaraca = 0;
     broj_zena = 0;
-    int broj_zaposlenih_muskaraca = 10;
-    int broj_zaposlenih_zena = 9;
-    int ukupan_broj_zaposlenih = broj_zaposlenih_muskaraca + broj_zaposlenih_zena;
-    printf("U firmi ima %d zaposlenih muskaraca, %d zena, ukupno %d zaposlenih\n",
-	    broj_zaposlenih_muskaraca, broj_zaposlenih_zena, ukupan_broj_zaposlenih);
-    sem_init(&x, 0, 1);  // postavljamo semafore na default vrednosti
+   
+    int ukupan_broj_zaposlenih = 19;
+    printf("Ukupno %d osoba\n", ukupan_broj_zaposlenih);
+       
+    // postavljamo semafore na default vrednosti
+    sem_init(&x, 0, 1);  
     sem_init(&y, 0, 1);
     sem_init(&z, 0, 1);
     sem_init(&muskarac_sem, 0, 1);
     sem_init(&zena_sem, 0, 1);
     sem_init(&max_sem, 0, MAX_OSOBA);  // postavljamo kapacitet toaleta
 
-    pthread_t *tid;
-    tid = malloc(80 * sizeof(pthread_t));
+    pthread_t *zaposleni;
+    zaposleni = malloc(80 * sizeof(pthread_t));
+
     int i;
-    for(i = 0; i < broj_zaposlenih_zena; i++){
-	pthread_create(&tid[i], NULL, zena, NULL);
+    
+    // iteracija kroz svaku osobu u firmi
+    for(i = 0; i < ukupan_broj_zaposlenih; i++){
+        // random broj za odabir pola osobe
+        float r = rand();
+        float random_pol = r / RAND_MAX;
+        //  printf("random: %f\n", random_pol);
+        if(random_pol > 0.5)
+             pthread_create(&zaposleni[i], NULL, zena, NULL);
+        else
+             pthread_create(&zaposleni[i], NULL, muskarac, NULL);
+        pauza();
     }
-    for(i = broj_zaposlenih_zena; i < ukupan_broj_zaposlenih; i++){     
-	pthread_create(&tid[i], NULL, muskarac, NULL);
-    }
+
     for(i = 0; i < ukupan_broj_zaposlenih; i++){     
-	pthread_join(tid[i], NULL);
+       pthread_join(zaposleni[i], NULL);
     }
+
     return 0;
 }
 
-void *zena(void *param)
-{
+void *zena(void *param){
     dolazak(1);
     sem_wait(&z);
     sem_wait(&zena_sem);
     sem_wait(&y);
     broj_zena++;
     if(broj_zena==1)
-	sem_wait(&muskarac_sem); 
+       sem_wait(&muskarac_sem); 
     sem_post(&y);
     sem_post(&zena_sem);
     sem_post(&z);
-
+ 
     sem_wait(&max_sem);
 
    // printf("Osoba zenskog pola je usla u wc\n");
-    pauza(1);
+    ulazak(1);
    // printf("Osoba zenskog pola je izasla iz wc-a.\n");
 
     sem_post(&max_sem);     
 
     sem_wait(&y);
     broj_zena--;
+
     if(broj_zena==0)
-	sem_post(&muskarac_sem); 
+       sem_post(&muskarac_sem); 
+
     sem_post(&y);
-}
+} 
 
 void *muskarac(void *param)
 {
@@ -97,22 +110,22 @@ void *muskarac(void *param)
     sem_wait(&x);
     broj_muskaraca++;
     if(broj_muskaraca==1)
-	sem_wait(&zena_sem); // jedan muskarac je u wc-u, wc dostupan samo muskarcima
+	   sem_wait(&zena_sem); // jedan muskarac je u wc-u, wc dostupan samo muskarcima
     sem_post(&x);
     sem_post(&muskarac_sem);
     sem_post(&z);
 
     sem_wait(&max_sem); // ukoliko je kapacitet wc-a popunjen, cekaj
 
-    // printf("Osoba muskog pola je usla u wc\n");
-    pauza(0);
-    // printf("Osoba muskog pola je izasla iz wc-a.\n");
+    ulazak(0);
 
     sem_post(&max_sem);
 
     sem_wait(&x);
+
     broj_muskaraca--;
+
     if(broj_muskaraca==0)
-	sem_post(&zena_sem);
+       sem_post(&zena_sem);
     sem_post(&x);
 }
